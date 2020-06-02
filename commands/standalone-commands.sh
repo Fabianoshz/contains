6ae2b87652bc3fe8a6908283de8e48d7c455fc29
Fabiano Honorato <fabianoshz@gmail.com>
2020-06-02 18:53:10 -0300

    Add pgp sig ignore on nginx install and small text fixes.

--- commands/standalone-commands.sh
+++ commands/standalone-commands.sh
@@ -1,10 +1,10 @@
-# Host: Boostrap Arch Linux SO
+# Host: Boostrap Arch Linux SO.
 mkdir system-root && sudo pacstrap system-root base base-devel
 
-# Host: Forked jail with namespaces
+# Host: Forked jail with namespaces.
 sudo unshare --mount --uts --ipc --net --pid --cgroup --fork chroot system-root bin/bash
 
-# Container: Mount /proc structure
+# Container: Mount /proc structure.
 mount -t proc none /proc
 
 # Host: Add virtual interface.
@@ -26,13 +26,14 @@ echo 'nameserver 8.8.8.8' > /etc/resolv.conf
 # Host: Enable routing
 sudo sysctl net.ipv4.conf.all.forwarding=1 # By default linux hosts don't forward IPv4 and IPv6 packets.
 sudo iptables -t nat -A POSTROUTING -s 10.1.0.0/24 -o <host-interface> -j MASQUERADE
-sudo iptables -A FORWARD -o <host-interface> -i host -j ACCEPT # Accept what comes from host interface to the virtual interface
-sudo iptables -A FORWARD -i <host-interface> -o host -j ACCEPT # Accept what comes from virtual interface to the host interface
+sudo iptables -A FORWARD -o <host-interface> -i host -j ACCEPT # Accept what comes from host interface to the virtual interface.
+sudo iptables -A FORWARD -i <host-interface> -o host -j ACCEPT # Accept what comes from virtual interface to the host interface.
 
 # Container: Mount root FS
 mount --bind / /
 
 # Container: Install and run nginx
+sed -i 's/SigLevel    = Required DatabaseOptional/SigLevel    = Never/' /etc/pacman.conf # Disable Pacman signature validation.
 pacman -S --noconfirm nginx
 nginx
 
